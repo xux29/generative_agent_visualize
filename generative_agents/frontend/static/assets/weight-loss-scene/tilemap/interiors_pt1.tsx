195129bf89c6ce6836608b5d91ba0454f288d322
Ge Yate <geyate@gmail.com>
2026-02-02 15:18:29 +0800

    更换减肥场景 ， 优化可视化

---
+++ generative_agents/frontend/static/assets/weight-loss-scene/tilemap/interiors_pt1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="interiors_pt1" tilewidth="32" tileheight="32" tilecount="5008" columns="16">
+ <image source="tileset/interiors_pt1.png" width="512" height="10016"/>
+</tileset>
---
+++ generative_agents/frontend/static/assets/weight-loss-scene/tilemap/interiors_pt4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="interiors_pt4" tilewidth="32" tileheight="32" tilecount="2000" columns="16">
+ <image source="tileset/interiors_pt4.png" width="512" height="4000"/>
+</tileset>
